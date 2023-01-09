b326ba632ad5c2f6acc8e62c60aea66d6d0b3e7f
astrocoding <zaenalalfian20@gmail.com>
2023-01-09 11:17:10 +0700

    refactor: add OTP empty condition

--- 006_otp_generator/script.js
+++ 006_otp_generator/script.js
@@ -1,15 +1,31 @@
 function generateOTP() {
   const otp = Math.floor(100000 + Math.random() * 900000); // 6 digit OTP
   document.getElementById('otp').value = otp;
+
+  document.getElementById('otp').disabled = false;
 }
 
 function copyOTP() {
   const otpInput = document.getElementById('otp');
-  otpInput.select();
-  otpInput.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-  alert('OTP Copied to Clipboard!');
+
+  if (otpInput.value !== '') {
+    otpInput.select();
+    otpInput.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    alert('OTP Copied to Clipboard!');
+  } else {
+    alert('Please Generate OTP First!');
+    document.getElementById('copy-btn').disabled = true;
+  }
+}
+
+function checkOTP() {
+  const otpInput = document.getElementById('otp');
+  const copyBtn = document.getElementById('copy-btn');
+
+  copyBtn.disabled = otpInput.value === '';
 }
 
 document.getElementById('generate-btn').addEventListener('click', generateOTP);
 document.getElementById('copy-btn').addEventListener('click', copyOTP);
+document.getElementById('otp').addEventListener('input', checkOTP);
