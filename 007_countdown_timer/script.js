5da0eea12eb4abcc76574e5e69011b3337d3acf5
astrocoding <zaenalalfian20@gmail.com>
2023-01-09 11:28:15 +0700

    add: script js to countdown timer

---
+++ 007_countdown_timer/script.js
@@ -0,0 +1,37 @@
+let countdownInterval;
+
+function startCountdown() {
+  const input = document.getElementById('date-input').value;
+  const endDate = new Date(input).getTime();
+
+  if (isNaN(endDate)) {
+    alert('Please select a valid date and time.');
+    return;
+  }
+
+  countdownInterval = setInterval(() => {
+    const now = new Date().getTime();
+    const timeLeft = endDate - now;
+
+    if (timeLeft < 0) {
+      clearInterval(countdownInterval);
+      alert('Countdown has ended!');
+      return;
+    }
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+  }, 1000);
+}
+
+document.getElementById('start-btn').addEventListener('click', () => {
+  clearInterval(countdownInterval);
+  startCountdown();
+});
